55d7a677489029b3dc3217c78b4275c20de2ec3f
gocuber <dafei.net@gmail.com>
2020-01-09 16:24:33 +0800

    cfop公式

--- res/js/cfop.js
+++ res/js/cfop.js
@@ -2,41 +2,640 @@ var cfopAlgs = {
   'oll1-a' : {
     'name':'OLL1',
     'algs':[
-      'RURURURURU1',
-      'RURURURURU2',
-      'RURURURURU3',
-    ],
-  },
-  'oll1-c' : {
-    'name':'OLL1',
-    'algs':[
-      'RURURURURU1',
-      'RURURURURU2',
-      'RURURURURU3',
-    ],
-  },
-  'oll1-b' : {
-    'name':'OLL1',
-    'algs':[
-      'RURURURURU1',
-      'RURURURURU2',
-      'RURURURURU3',
-    ],
-  },
-  'oll1-d' : {
-    'name':'OLL1',
-    'algs':[
-      'RURURURURU1',
-      'RURURURURU2',
-      'RURURURURU3',
+      '(RU\'2)(R\'2FRF\')U2(R\'FRF\')',
     ],
   },
   'oll2-a' : {
     'name':'OLL2',
     'algs':[
-      'RURURURURU1',
-      'RURURURURU2',
-      'RURURURURU3',
+      'F(RUR\'U\')F\'f(RUR\'U\')f\'',
+    ],
+  },
+  'oll3-a' : {
+    'name':'OLL3',
+    'algs':[
+      'f(RUR\'U\')f\'U\'F(RUR\'U\')F\'',
+    ],
+  },
+  'oll4-a' : {
+    'name':'OLL4',
+    'algs':[
+      'f(RUR\'U\')f\'UF(RUR\'U\')F\'',
+    ],
+  },
+  'oll17-a' : {
+    'name':'OLL17',
+    'algs':[
+      '(RUR\'U)(R\'FRF\')U2(R\'FRF\')',
+      'f(RUR\'U\')f\'U\'(RUR\'U\')(R\'FRF\')',
+    ],
+  },
+  'oll18-a' : {
+    'name':'OLL18',
+    'algs':[
+      '(FRUR\'d)(R\'U2)(R\'FRF\')',
+      '(FRUR\'U)y\'(R\'U2)(R\'FRF\')',
+    ],
+  },
+  'oll19-a' : {
+    'name':'OLL19',
+    'algs':[
+      'r\'RU(RUR\'U\')(rR\'2FRF\')',
+      'MU(RUR\'U\')M\'(R\'FRF\')',
+    ],
+  },
+  'oll37-a' : {
+    'name':'OLL37',
+    'algs':[
+      'F(RU\'R\'U\')(RUR\'F\')',
+    ],
+  },
+  'oll33-a' : {
+    'name':'OLL33',
+    'algs':[
+      '(RUR\'U\')(R\'FRF\')',
+    ],
+  },
+  'oll28-a' : {
+    'name':'OLL28',
+    'algs':[
+      '(rUR\'U\')r\'R(URU\'R\')',
+      '(rUR\'U\')M(URU\'R\')',
+    ],
+  },
+  'oll57-a' : {
+    'name':'OLL57',
+    'algs':[
+      '(RUR\'U\')rR\'(URU\'r\')',
+      '(RUR\'U\')M\'(URU\'r\')',
+      '(M\'U)2M\'U2(MU)2M',
+    ],
+  },
+  'oll20-a' : {
+    'name':'OLL20',
+    'algs':[
+      'r\'RU(RUR\'U\')M\'2(URU\'r\')',
+      '(rUR\'U\')M\'2(URU\'R\')U\'M\'',
+      '(M\'U)4(MU)4',
+    ],
+  },
+  'oll21-a' : {
+    'name':'OLL21',
+    'algs':[
+      '(R\'U2)(RUR\'U\')(RUR\'UR)',
+      '(RU\'2R\'U\')(RUR\'U\')(RU\'R\')',
+      'F(RUR\'U\')3F\'',
+      'F(URU\'R\')3F\'',
+    ],
+  },
+  'oll21-b' : {
+    'name':'OLL21',
+    'algs':[
+      '(R\'U\')(RU\'R\'URU\')(R\'U2R)',
+      '(RUR\'U)(RU\'R\'U)(RU\'2R\')',
+    ],
+  },
+  'oll22-a' : {
+    'name':'OLL22',
+    'algs':[
+      '(RU\'2)(R\'2U\')(R2U\')(R\'2U2R)',
+      'f(RUR\'U\')f\'F(RUR\'U\')F',
+    ],
+  },
+  'oll23-a' : {
+    'name':'OLL23',
+    'algs':[
+      '(R\'2D\')(RU2R\'D)(RU2R)',
+    ],
+  },
+  'oll24-a' : {
+    'name':'OLL24',
+    'algs':[
+      '(rUR\'U\')(r\'FRF\')',
+    ],
+  },
+  'oll25-a' : {
+    'name':'OLL25',
+    'algs':[
+      'F\'(rUR\'U\')(r\'FR)',
+    ],
+  },
+  'oll26-a' : {
+    'name':'OLL26',
+    'algs':[
+      '(RU\'2)(R\'U\'RU\'R\')',
+    ],
+  },
+  'oll26-b' : {
+    'name':'OLL26',
+    'algs':[
+      '(R\'U\'RU\')(R\'U2R)',
+    ],
+  },
+  'oll27-a' : {
+    'name':'OLL27',
+    'algs':[
+      '(R\'U2)(RUR\'UR)',
+    ],
+  },
+  'oll27-b' : {
+    'name':'OLL27',
+    'algs':[
+      '(RUR\'U)(RU\'2R\')',
+    ],
+  },
+  'oll5-a' : {
+    'name':'OLL5',
+    'algs':[
+      '(r\'U2)(RUR\'Ur)',
+    ],
+  },
+  'oll5-b' : {
+    'name':'OLL5',
+    'algs':[
+      '(R\'F2)(rUr\'FR)',
+    ],
+  },
+  'oll6-a' : {
+    'name':'OLL6',
+    'algs':[
+      '(rU\'2)(R\'U\'RU\'r\')',
+    ],
+  },
+  'oll7-a' : {
+    'name':'OLL7',
+    'algs':[
+      '(rUR\'U)(RU\'2r\')',
+    ],
+  },
+  'oll8-a' : {
+    'name':'OLL8',
+    'algs':[
+      '(r\'U\'RU\')(R\'U2r)',
+    ],
+  },
+  'oll8-b' : {
+    'name':'OLL8',
+    'algs':[
+      '(RU\'2R\'U2)(R\'FRF\')',
+    ],
+  },
+  'oll9-a' : {
+    'name':'OLL9',
+    'algs':[
+      '(RUR\'U\')(R\'FR2)(UR\'U\'F\')',
+      '(R\'U\'R)z\'y\'(RU\'R\'F)(RUR\')',
+    ],
+  },
+  'oll9-b' : {
+    'name':'OLL9',
+    'algs':[
+      '(R\'U\'RU\')(R\'U)(R\'FRF\')(UR)',
+    ],
+  },
+  'oll10-a' : {
+    'name':'OLL10',
+    'algs':[
+      '(RUR\'U)(R\'FRF\')(RU\'2R\')',
+    ],
+  },
+  'oll11-a' : {
+    'name':'OLL11',
+    'algs':[
+      'M(RUR\'U)(RU\'2R\')UM\'',
+      'r\'(R2UR\'U)(RU\'2)(R\'UM\')',
+    ],
+  },
+  'oll11-b' : {
+    'name':'OLL11',
+    'algs':[
+      '(rUR\'U)(R\'FRF\')(RU\'2r\')',
+    ],
+  },
+  'oll12-a' : {
+    'name':'OLL12',
+    'algs':[
+      'M\'(R\'U\'RU\')(R\'U2R)U\'M',
+    ],
+  },
+  'oll12-b' : {
+    'name':'OLL12',
+    'algs':[
+      'F(RUR\'U\')F\'UF(RUR\'U\')F\'',
+    ],
+  },
+  'oll13-a' : {
+    'name':'OLL13',
+    'algs':[
+      'F(URU\'R\'2)F\'R(URU\'R\')',
+      '(rU\'r\'U\')(rUr\')y\'R\'UR',
+    ],
+  },
+  'oll14-a' : {
+    'name':'OLL14',
+    'algs':[
+      'R\'F(RUR\'F\'R)y\'RU\'R\'',
+    ],
+  },
+  'oll15-a' : {
+    'name':'OLL15',
+    'algs':[
+      '(r\'U\'r)(R\'U\'RU)(r\'Ur)',
+    ],
+  },
+  'oll16-a' : {
+    'name':'OLL16',
+    'algs':[
+      '(rUr\')(RUR\'U\')(rU\'r\')',
+    ],
+  },
+  'oll31-a' : {
+    'name':'OLL31',
+    'algs':[
+      'R\'U\'F(URU\'R\')F\'R',
+    ],
+  },
+  'oll32-a' : {
+    'name':'OLL32',
+    'algs':[
+      'S(RUR\'U\')R\'FRf\'',
+    ],
+  },
+  'oll34-a' : {
+    'name':'OLL34',
+    'algs':[
+      '(RUR2U\')(R\'FRU)(RU\'F\')',
+      '(RUR\'U\')y\'(r\'U\'RU)M\'',
+    ],
+  },
+  'oll35-a' : {
+    'name':'OLL35',
+    'algs':[
+      '(RU\'2)(R\'2FRF\')(RU\'2R\')',
+    ],
+  },
+  'oll36-a' : {
+    'name':'OLL36',
+    'algs':[
+      '(RUR\'F\')(RUR\'U\')(R\'FRU\')(R\'FRF\')',
+    ],
+  },
+  'oll38-a' : {
+    'name':'OLL38',
+    'algs':[
+      '(RUR\'U)(RU\'R\'U\')(R\'FRF\')',
+    ],
+  },
+  'oll39-a' : {
+    'name':'OLL39',
+    'algs':[
+      '(rU\'r\'U\'r)y(RUR\'f\')',
+    ],
+  },
+  'oll39-b' : {
+    'name':'OLL39',
+    'algs':[
+      '(RUR\'F\')(U\'FU)(RU\'2R\')',
+    ],
+  },
+  'oll40-a' : {
+    'name':'OLL40',
+    'algs':[
+      '(R\'F)(RUR\'U\'F\')(UR)',
+    ],
+  },
+  'oll29-a' : {
+    'name':'OLL29',
+    'algs':[
+      '(RUR\'U\')(RU\'R\')(F\'U\'F)(RUR\')',
+    ],
+  },
+  'oll30-a' : {
+    'name':'OLL30',
+    'algs':[
+      '(R\'U\'RU\')(R\'U)(R\'FRF\')2(UR)',
+    ],
+  },
+  'oll41-a' : {
+    'name':'OLL41',
+    'algs':[
+      '(RUR\'U)(RU\'2R\')F(RUR\'U\')F\'',
+    ],
+  },
+  'oll42-a' : {
+    'name':'OLL42',
+    'algs':[
+      '(R\'U\'RU\')(R\'U2R)F(RUR\'U\')F\'',
+    ],
+  },
+  'oll44-a' : {
+    'name':'OLL44',
+    'algs':[
+      'f(RUR\'U\')f\'',
+    ],
+  },
+  'oll44-b' : {
+    'name':'OLL44',
+    'algs':[
+      'F(URU\'R\')F\'',
+    ],
+  },
+  'oll45-a' : {
+    'name':'OLL45',
+    'algs':[
+      'F(RUR\'U\')F\'',
+    ],
+  },
+  'oll45-b' : {
+    'name':'OLL45',
+    'algs':[
+      'f(URU\'R\')f\'',
+    ],
+  },
+  'oll43-a' : {
+    'name':'OLL43',
+    'algs':[
+      '(B\'U\')(R\'URB)',
+    ],
+  },
+  'oll46-a' : {
+    'name':'OLL46',
+    'algs':[
+      '(R\'U\')(R\'FRF\')(UR)',
+    ],
+  },
+  'oll47-a' : {
+    'name':'OLL47',
+    'algs':[
+      '(R\'U\')(R\'FRF\')2(UR)',
+      '(R\'U\')x(R\'URU\')2x\'(UR)',
+    ],
+  },
+  'oll48-a' : {
+    'name':'OLL48',
+    'algs':[
+      'F(RUR\'U\')2F\'',
+    ],
+  },
+  'oll48-b' : {
+    'name':'OLL48',
+    'algs':[
+      'f(URU\'R\')2f\'',
+    ],
+  },
+  'oll49-a' : {
+    'name':'OLL49',
+    'algs':[
+      '(RB\'R\'2F)(R2BR\'2F\'R)',
+    ],
+  },
+  'oll49-b' : {
+    'name':'OLL49',
+    'algs':[
+      '(RU\'2)(R\'U\'RU\')(R\'2U\'F)(R\'F\'RUR)',
+    ],
+  },
+  'oll50-a' : {
+    'name':'OLL50',
+    'algs':[
+      '(R\'FR2B\')(R\'2F\'R2BR\')',
+    ],
+  },
+  'oll50-b' : {
+    'name':'OLL50',
+    'algs':[
+      '(RU\'2)(R\'U\'RU\'R\')F(RUR\'U\')F\'',
+    ],
+  },
+  'oll50-c' : {
+    'name':'OLL50',
+    'algs':[
+      'r\'U(r2U\'r\'2U\')r2Ur\'',
+    ],
+  },
+  'oll53-a' : {
+    'name':'OLL53',
+    'algs':[
+      'r\'U\'(RU\'R\'URU\')(R\'U2r)',
+    ],
+  },
+  'oll53-b' : {
+    'name':'OLL53',
+    'algs':[
+      'r\'U2(RUR\'U\')(RUR\'Ur)',
+    ],
+  },
+  'oll53-c' : {
+    'name':'OLL53',
+    'algs':[
+      'F(RUR\'U\')F\'(RUR\'U\')(R\'FRF\')',
+    ],
+  },
+  'oll54-a' : {
+    'name':'OLL54',
+    'algs':[
+      '(rUr\')(RUR\'U\')2(rU\'r\')',
+      '(rU\'2R\'U\')(RUR\'U\')(RU\'r\')',
+    ],
+  },
+  'oll51-a' : {
+    'name':'OLL51',
+    'algs':[
+      'f(RUR\'U\')2f\'',
+    ],
+  },
+  'oll51-b' : {
+    'name':'OLL51',
+    'algs':[
+      'F(URU\'R\')2F\'',
+    ],
+  },
+  'oll52-a' : {
+    'name':'OLL52',
+    'algs':[
+      '(R\'F\'U\'FU\')(RUR\'UR)',
+    ],
+  },
+  'oll52-b' : {
+    'name':'OLL52',
+    'algs':[
+      '(R\'U\'RU\')(R\'U)y\'(R\'URB)',
+    ],
+  },
+  'oll55-a' : {
+    'name':'OLL55',
+    'algs':[
+      '(R\'FRURU\')(R\'2F\'R2U\'R\'U)(RUR\')',
+      'R\'F(URU\')(R\'2F\'R2)(UR\'U\'R)',
+    ],
+  },
+  'oll56-a' : {
+    'name':'OLL56',
+    'algs':[
+      '(rUr\')(URU\'R\')2(rU\'r\')',
+    ],
+  },
+
+  'pll1-a' : {
+    'name':'PLL1-Ua',
+    'algs':[
+      '(RU\'R)U(RURU\')(R\'U\'R2)',
+      'M\'2U(MU2)(M\'U)M\'2',
+    ],
+  },
+  'pll2-a' : {
+    'name':'PLL2-Ub',
+    'algs':[
+      '(R\'2U)(RUR\'U\')(R\'U\')(R\'UR\')',
+      'M\'2U\'(MU2)(M\'U\')M\'2',
+    ],
+  },
+  'pll3-a' : {
+    'name':'PLL3-H',
+    'algs':[
+      '(M\'2U)(M\'2U2)(M\'2U)M\'2',
+    ],
+  },
+  'pll4-a' : {
+    'name':'PLL4-Z',
+    'algs':[
+      '(M\'2U)2(M\'U2)(M\'2U2)(M\'U2)',
+      '(UR\'U\')(RU\'R)U(RU\'R\'U)(RU)(R2U\'R\'U)',
+    ],
+  },
+  'pll4-b' : {
+    'name':'PLL4-Z',
+    'algs':[
+      '(M\'U)(M\'2U)2(M\'U2)(M\'2U\')',
+      '(R\'U\')(RU\'R)U(RU\'R\'U)(RU)(R2U\'R\')U2',
+    ],
+  },
+  'pll5-a' : {
+    'name':'PLL5-Aa',
+    'algs':[
+      'x(R\'UR\')D2(RU\'R\')(D2R2)',
+    ],
+  },
+  'pll5-b' : {
+    'name':'PLL5-Aa',
+    'algs':[
+      'x\'(R2D2)(R\'U\'R)D2(R\'UR\')',
+    ],
+  },
+  'pll6-a' : {
+    'name':'PLL6-Ab',
+    'algs':[
+      'x(R\'2D2)(RUR\')D2(RU\'R)',
+    ],
+  },
+  'pll6-b' : {
+    'name':'PLL6-Ab',
+    'algs':[
+      'x\'(RU\'R)D2(R\'UR)D2R\'2',
+    ],
+  },
+  'pll7-a' : {
+    'name':'PLL7-E',
+    'algs':[
+      'x\'(RU\'R\'D)(RUR\'D\')(RUR\'D)(RU\'R\'D\')',
+    ],
+  },
+  'pll8-a' : {
+    'name':'PLL8-T',
+    'algs':[
+      '(RUR\'U\')(R\'FR2U\'R\'U\')(RUR\'F\')',
+    ],
+  },
+  'pll9-a' : {
+    'name':'PLL9-F',
+    'algs':[
+      '(R\'U\'F\')(RUR\'U\')(R\'FR2U\'R\'U\')(RUR\'UR)',
+      '(U\'R\'URU\')(R\'l\'d\')(RUR\')y\'(RUR\'U\')(l2)',
+    ],
+  },
+  'pll10-a' : {
+    'name':'PLL10-V',
+    'algs':[
+      '(R\'UR\'U\')y(R\'F\'R2U\')(R\'UR\'F)(RF)',
+    ],
+  },
+  'pll11-a' : {
+    'name':'PLL11-Y',
+    'algs':[
+      '(FRU\'R\'U\')(RUR\'F\')(RUR\'U\')(R\'FRF\')',
+    ],
+  },
+  'pll12-a' : {
+    'name':'PLL12-Ja',
+    'algs':[
+      'x(R\'2FRF\')(RU\'2)(r\'Ur)U2',
+    ],
+  },
+  'pll12-b' : {
+    'name':'PLL12-Ja',
+    'algs':[
+      '(L\'UR\')z(R2UR\'U\')(R2UDR\')',
+    ],
+  },
+  'pll13-a' : {
+    'name':'PLL13-Jb',
+    'algs':[
+      '(RUR\'F\')(RUR\'U\')(R\'FR2U\'R\'U\')',
+    ],
+  },
+  'pll14-a' : {
+    'name':'PLL14-Rb',
+    'algs':[
+      '(R\'U2)(RU\'2)(R\'F)(RUR\'U\')(R\'F\'R2U\')',
+    ],
+  },
+  'pll15-a' : {
+    'name':'PLL15-Ra',
+    'algs':[
+      '(RU\'2)(R\'U2)(RB\')(R\'U\'RURBR\'2U)',
+    ],
+  },
+  'pll15-b' : {
+    'name':'PLL15-Ra',
+    'algs':[
+      '(RU\'R\'U\')(RURD)(R\'U\'RD\')(R\'U2R\'U\')',
+      '(RUR\'F\')(RU\'2)(R\'U2)(R\'FRU)(RU\'2R\'U\')',
+    ],
+  },
+  'pll16-a' : {
+    'name':'PLL16-Gc',
+    'algs':[
+      '(R\'2u\'RU\'R)(UR\'uR2)y(RU\'R\')',
+    ],
+  },
+  'pll17-a' : {
+    'name':'PLL17-Gd',
+    'algs':[
+      '(RUR\')y\'(R2u\'RU\')(R\'UR\'uR2)',
+    ],
+  },
+  'pll18-a' : {
+    'name':'PLL18-Ga',
+    'algs':[
+      '(R2uR\')(UR\'U\'Ru\')(R\'2F\'UF)',
+    ],
+  },
+  'pll19-a' : {
+    'name':'PLL19-Gb',
+    'algs':[
+      '(R\'d\'F)(R2uR\'U)(RU\'Ru\'R\'2)',
+    ],
+  },
+  'pll20-a' : {
+    'name':'PLL20-Nb',
+    'algs':[
+      '(R\'URU\')(R\'F\'U\'F)(RUR\'F)(R\'F\'RU\'R)',
+    ],
+  },
+  'pll21-a' : {
+    'name':'PLL21-Na',
+    'algs':[
+      '(RUR\'U)(RUR\'F\')(RUR\'U\')(R\'F)(R2U\'R\'U2)(RU\'R\')',
     ],
   },
 };
@@ -53,7 +652,7 @@ function cfopAlg() {
       var key = $(this).data('id');
       var alg = algs[key];
       $(this).children('.name').text(alg.name);
-      $(this).children('.pic').html('<img src="/res/images/cfop/' + key + '.jpg">');
+      // $(this).children('.pic').html('<img src="/res/images/cfop/' + key + '.jpg">');
       $(this).children('.algs').html(alg.algs.join('<br>'));
     });
 	};
